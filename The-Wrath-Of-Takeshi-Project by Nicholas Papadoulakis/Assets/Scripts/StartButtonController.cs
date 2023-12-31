da989ac3398769b7c9ca7d456c9b52af2848fd35
np1997productions <130023981+np1997productions@users.noreply.github.com>
2023-12-30 19:14:52 -0500

    d

---
+++ The-Wrath-Of-Takeshi-Project by Nicholas Papadoulakis/Assets/Scripts/StartButtonController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButtonController : MonoBehaviour
+{
+    public Button myButton;
+    public float appearanceDelay = 4f;
+    public float disappearanceDelay = 20f;
+    public string nextScene = "NextScene";
+
+    void Start()
+    {
+        myButton.gameObject.SetActive(false); // Ensure the button is initially hidden
+
+        Invoke("ShowButton", appearanceDelay);
+        Invoke("HideButton", disappearanceDelay);
+    }
+
+    void ShowButton()
+    {
+        myButton.gameObject.SetActive(true);
+    }
+
+    void HideButton()
+    {
+        myButton.gameObject.SetActive(false);
+    }
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
+
